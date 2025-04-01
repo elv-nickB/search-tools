8bcda7c2dca4f01a47c9c1f3179cc77a8d210bcb
Nicholas Buoncristiani <nick.buoncristiani@eluv.io>
2025-04-01 17:50:35 +0000

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='search_utils',
     version='0.1',
-    requires=['loguru==0.7.2'],
+    install_requires=['loguru',
+                      'tqdm'],
     packages=find_packages(),
 )
